bcb14016f473cc594f39d6b0e2af8730ceb2f78e
Claude Myburgh <claudemyburgh@gmail.com>
2024-07-06 00:13:09 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,20 @@ import dts from "vite-plugin-dts"
 import { resolve } from "path"
 
 export default defineConfig({
+    test: {
+    alias: {
+      "@/": resolve(__dirname, "src"),
+    },
+    coverage: {
+      lines: 60,
+      branches: 60,
+      functions: 60,
+      statements: 60,
+      provider: "istanbul",
+      reporter: ["json-summary", "text", "json"],
+      reportOnFailure: true
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
